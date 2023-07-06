ea57c961c93bf1881e2e7ed7a86ac9bbe70c7fcf
Donnie46 <122732624+Donnie46@users.noreply.github.com>
2023-07-06 10:31:00 -0500

    added User.js model info

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "does not match email:"]
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      }
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return `${this.friends.length}`;
+  })
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
