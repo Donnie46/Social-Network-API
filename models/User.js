19629085de36809d87e2ed72a631e690e0431508
Donnie46 <122732624+Donnie46@users.noreply.github.com>
2023-08-07 13:10:00 -0500

    bug fixes user.js models

--- models/User.js
+++ models/User.js
@@ -12,35 +12,32 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "does not match email!"]
+      match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
     },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
         ref: "Thought",
-      }
+      },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
         ref: "User",
-      }
+      },
     ],
   },
   {
     toJSON: {
-      getters: true,
+      virtuals: true,
     },
     id: false,
-  }
+  },
 );
 
-userSchema
-  .virtual('friendCount')
-  // Getter method
-  .get(function () {
-    return `${this.friends.length}`;
-  })
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
 
 
 const User = model('user', userSchema);
